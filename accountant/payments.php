7b925bc19aa57b38e63b2c174519d16b9a61588b
Vishakha0121 <112022967+Vishakha0121@users.noreply.github.com>
2022-12-29 11:36:53 +0530

    Update payments.php

--- accountant/payments.php
+++ accountant/payments.php
@@ -1,16 +1,48 @@
-<?php include '../config.php'; ?>
+<?php include('../config.php');?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home | ERP Model</title>
+        <link rel="stylesheet" href="./assets/css/base-styles.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<style>
+			input[type=checkbox]
+			{
+			  /* Double-sized Checkboxes */
+			  -ms-transform: scale(1.3); /* IE */
+			  -moz-transform: scale(1.3); /* FF */
+			  -webkit-transform: scale(1.3); /* Safari and Chrome */
+			  -o-transform: scale(1.3); /* Opera */
+			  transform: scale(1.3);
+			  padding: 10px;
+			  cursor:pointer;
+			}
+			.card {
+  				box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+ 
+			}
+		</style>
+    </head>
+
+	<body>
+
 <div class="container-fluid">
 	<div class="col-lg-12">
-		<div class="row mb-4 mt-4">
-			<div class="col-md-12">
-				
-			</div>
-		</div>
+		
 		<div class="row">
 			<div class="col-md-12">
 				<div class="card">
-					<div class="card-header">
-						<b>List of Payments </b>
+					<div class="card-header"style="background-color:#1d3b55">
+						<b  style="color:white">List of Payments </b>
 						<span class="float:right"><a class="btn btn-primary btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_payment">
 					<i class="fa fa-plus"></i> New 
 				</a></span>
@@ -18,13 +50,13 @@
 					<div class="card-body">
 						<table class="table table-condensed table-bordered table-hover">
 							<thead>
-								<tr>
-									<th class="text-center">#</th>
-									<th class="">Date</th>
-									<th class="">Student Id</th>
-									<th class="">EF No.</th>
-									<th class="">Name</th>
-									<th class="">Paid Amount</th>
+								<tr style="background-color:#87CEFA">
+									<th class="text-center">S.No.</th>
+									<th class="text-center">Date</th>
+									<th class="text-center">Student Id</th>
+									<th class="text-center">EF No.</th>
+									<th class="text-center">Name</th>
+									<th class="text-center">Paid Amount</th>
 									<th class="text-center">Action</th>
 								</tr>
 							</thead>
@@ -79,6 +111,56 @@
 		</div>
 	</div>
 </div>
+<br>
+<footer id="footer resources" class="p-4" style="background-color: rgba(0, 0, 0, 0.7);color:#fff;text-align:center">
+    <div class="container-fluid" id="inner-footer" >
+        <div class="row">
+            <div class="col-md-2 row-col">
+                <ul type="none"> 
+                    <h4 style="color:white"><b>Timetable</b></h4>
+                    <li><a href="./timetable.php" style="color:#89CFF0">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Syllabus</b></h4>
+                    <li><a href="./syllabus.php" style="color:#89CFF0">View by Standard</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Explore</b></h4>
+                    <li><a href="../index.php" style="color:#89CFF0">Homepage</a></li>
+                    <li><a href="#!" style="color:#89CFF0">TEQIP III</a></li>
+                    <li><a href="#!" style="color:#89CFF0">Training & Placements</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Social Media Links</b></h4>
+                    <li><a href="https://www.facebook.com/swaraj4715" style="color:#89CFF0">Facebook</a></li>
+                    <li><a href="https://www.facebook.com/swaraj4715" style="color:#89CFF0">Instagram</a></li>
+                    <li><a href="https://twitter.com/swaraj1729" style="color:#89CFF0">Twitter</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 row-col">
+                <ul type="none">
+                    <h4 style="color:white"><b>Contact Us</b></h4>
+                    <li style="color:white"><span>School Enterprise Resource Planning Platform, Yantromitra Solutions Private Limited</span></li><br>
+                    <li><a href="https://goo.gl/maps/y3ZwTV9u2sweudjA6" target="_blank" style="color:#89CFF0"><span>Sikkim, Ravangla, Sikkim, India - 737139</span></a></li>
+                    <li><a href="tel:+91 8002046457" style="color:#89CFF0"><span>+91 8002046457</span></a></li>
+                    <li><a href="mailto:swarajkumarchaudhary1729@gmail.com" style="color:#89CFF0"><span>swarajkumarchaudhary1729@gmail.com</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright mb-3">
+        <b>&copy; Swaraj Kumar Chaudhary (2021)</b>
+    </div>
+</footer>
+        <br>
+</body>
+</html>
 <style>
 	
 	td{
