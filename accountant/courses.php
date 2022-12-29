339ccc1c55798adf5aa3ea8489c904df76fa54ec
Vishakha0121 <112022967+Vishakha0121@users.noreply.github.com>
2022-12-29 11:34:23 +0530

    Update courses.php

--- accountant/courses.php
+++ accountant/courses.php
@@ -30,97 +30,7 @@
     </head>
 
 	<body>
-		<div class="container-fluid">
-		<nav class="navbar navbar-expand-md" style="background-color:rgba(0, 0, 0, 0.7);color:#fff;border-color: #080808">
-    <p class="text-primary"><a class="navbar-brand" href="../index.php"><b>ERP Model.</b></a></p>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <i class="fa fa-bars text-primary" aria-hidden="true"></i>
-    </button>
-
-    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-        <?php
-
-        if ($_SESSION["user_category"] == "guest") {
-            echo '
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-primary" href="#our-services"><b style="color:white">Our Services</b></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-primary" href="#about-us"><b style="color:white">About Us</b></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-primary" href="./login.php"><b style="color:white">Login</b></a>
-                    </li>
-                </ul>
-            ';
-        }
-
-        if ($_SESSION["user_category"] == "admin") {
-            echo '
-                <ul class="navbar-nav text-center">
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./admin/admin.php"><b style="color:white">Dashboard</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link text-primary" href="./logout.php"><b style="color:white">Logout</b></a>
-                    </li>
-                </ul>
-            ';
-        }
-
-        if ($_SESSION["user_category"] == "student") {
-            echo '
-                <ul class="navbar-nav text-center">
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./student/dashboard.php"><b style="color:white">Dashboard</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./logout.php"><b style="color:white">Logout<b></a>
-                    </li>
-                </ul>
-            ';
-        }
-
-        if ($_SESSION["user_category"] == "teacher") {
-            echo '
-                <ul class="navbar-nav text-center">
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./teacher/grade.php"><b style="color:white">Grade</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="../teacher/attendance.php"><b style="color:white">Attendance</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="../profile.php"><b style="color:white">Profile</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="../logout.php"><b style="color:white">Logout</b></a>
-                    </li>
-                </ul>
-            ';
-        }
-
-        if ($_SESSION["user_category"] == "accountant") {
-            echo '
-                <ul class="navbar-nav text-center">
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./accountant/index.php"><b style="color:white">Dashboard</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link" href="./accountant/index.php"><b style="color:white">Fees, Payments & Receipts</b></a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <a class="nav-link text-primary" href="./logout.php"><b style="color:white">Logout</b></a>
-                    </li>
-                </ul>
-            ';
-        }
-        ?>
-    </div>
-</nav>
-
+        <div class="container-fluid">
 			<div class="col-lg-12">
 				<div class="row mb-4 mt-4">
 					<div class="col-md-12">
@@ -133,18 +43,17 @@
 					<!-- Table Panel -->
 					<div class="col-md-12">
 						<div class="card">
-							<div class="card-header">
-								<b>List of Courses and Fees</b>
-
+							<div class="card-header" style="background-color:#1d3b55">
+						        <b  style="color:white">List of Courses and Fees</b>
 							</div>
 							<div class="card-body">
 								<table class="table table-condensed table-bordered table-hover">
 									<thead>
-										<tr>
+										<tr style="background-color:#87CEFA">
 											<th class="text-center">S.No.</th>
-											<th class="">Standard</th>
-											<th class="">Description</th>
-											<th class="">Total Fee</th>
+											<th class="text-center">Standard</th>
+											<th class="text-center">Description</th>
+											<th class="text-center">Total Fee</th>
 											<th class="text-center">Action</th>
 										</tr>
 									</thead>
